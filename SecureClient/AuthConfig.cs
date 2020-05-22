09708be45f12609bdd28ea00a9e36fcfd5cd7a55
Chandan Kumar <chandan061184@outlook.com>
2020-05-22 23:47:13 +0530

    Call the secure api

---
+++ SecureClient/AuthConfig.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace SecureClient
+{
+    public class AuthConfig
+    {
+        public string Instance { get; set; } = "https://login.microsoftonline.com/{0}";
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string Authority
+        {
+            get
+            {
+                return String.Concat(CultureInfo.DefaultThreadCurrentCulture, Instance, TenantId);
+            }
+        }
+        // Function expresssion
+        //public string Authority => String.Format(CultureInfo.InvariantCulture, Instance, TenantId);
+
+
+        public string ClientSecret { get; set; }
+        public string BaseAddress { get; set; }
+        public string ResourceID { get; set; }
+
+        public static AuthConfig ReadFromJsonFile(string path)
+        {
+            IConfiguration Configuration;
+
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile(path);
+
+            Configuration = builder.Build();
+
+            return Configuration.Get<AuthConfig>();
+        }
+    }
+}
+
